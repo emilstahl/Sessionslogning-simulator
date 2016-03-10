f1183a674b80469015fb662ec8de2f05b059a58a
Morten Scheel <morten@mortenscheel.com>
2016-03-10 19:48:43 +0100

    Added dynamic messages, adjusted timing, fixed typo in manifest.json

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 var imgURL = chrome.extension.getURL("img/soren.png");
+var host = window.document.location.hostname;
 
 $('body').append('<div class="zombiepind" style="position:fixed; bottom:0px; z-index:10;"><img id="zombiebillede" src="http://i.imgur.com/cWuWl5O.png"/></div>');
 function pindLeft() {
@@ -10,13 +11,21 @@ function pindRight() {
 pindRight();
 $('.zombiepind').prepend("<div class='triangle-isosceles'>HVAD LAVER DU?</div>");
 
-$(function() {
+$(function () {
 
-   var text = ['HVAD LAVER DU?', 'Jeg holder øje med dig.', 'Er du måske terrorist?', 'Hvad er det dog, du kigger på?', 'Dit besøg er registreret.'];
-   var time = setInterval(function() {
-       var newText = text[Math.floor(Math.random()*text.length)];
-       $('.triangle-isosceles').text(newText);
-   },1000);
+    var text = [
+        'HVAD LAVER DU?',
+        'Jeg holder øje med dig.',
+        'Er du måske terrorist?',
+        'Hvad er det dog, du kigger på?',
+        'Dit besøg er registreret.',
+        'Hmmmm... ' + host + '. Det er hermed noteret.',
+        'Lige nu er der ' + (Math.floor(Math.random() * 10000)) + ' danskere på ' + host + '! Hvad laver I?'
+    ];
+    var time = setInterval(function () {
+        var newText = text[Math.floor(Math.random() * text.length)];
+        $('.triangle-isosceles').text(newText);
+    }, 3000);
 
 });
-document.getElementById("zombiebillede").src = imgURL;
+document.getElementById("zombiebillede").src = imgURL
